874c57a0a517760570261bbf02b50c2f55625a6d
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-06-17 00:21:57 +0300

    commit tree 6128da5d10521981c03ba449470d95b29923a57d

--- index.js
+++ index.js
@@ -1,1 +1,70 @@
 // Your code here
+const createEmployeeRecord = (array) => {
+  return {
+    firstName: array[0],
+    familyName: array[1],
+    title: array[2],
+    payPerHour: array[3],
+    timeInEvents: [],
+    timeOutEvents: [],
+  };
+};
+
+const createEmployeeRecords = function (employeeArrayData) {
+  return employeeArrayData.map(function (array) {
+    return createEmployeeRecord(array);
+  });
+};
+
+function createTimeInEvent(employeeRecord, dateStamp) {
+  let [date, hour] = dateStamp.split(" ");
+  employeeRecord.timeInEvents.push({
+    type: "TimeIn",
+    hour: parseInt(hour, 10),
+    date,
+  });
+  return employeeRecord;
+}
+
+const createTimeOutEvent = (employeeRecord, dateStamp) => {
+  let [date, hour] = dateStamp.split(" ");
+  employeeRecord.timeOutEvents.push({
+    type: "TimeOut",
+    hour: parseInt(hour, 10),
+    date,
+  });
+  return employeeRecord;
+};
+
+function hoursWorkedOnDate(employeeRecord, specificDate) {
+  let inEventDate = employeeRecord.timeInEvents.find(
+    (x) => x.date === specificDate
+  );
+  let outEventDate = employeeRecord.timeOutEvents.find(
+    (x) => x.date === specificDate
+  );
+
+  return (outEventDate.hour - inEventDate.hour) / 100; // (200 / 2)
+}
+
+const wagesEarnedOnDate = (employeeRecord, specificDate) => {
+  let wageOnDate =
+    hoursWorkedOnDate(employeeRecord, specificDate) * employeeRecord.payPerHour;
+  return wageOnDate;
+};
+const allWagesFor = (employeeRecord) => {
+  let eligibleDates = employeeRecord.timeInEvents.map(function (x) {
+    return x.date;
+  });
+
+  let grandWages = eligibleDates.reduce(function (accum, specificDate) {
+    return accum + wagesEarnedOnDate(employeeRecord, specificDate);
+  }, 0);
+
+  return grandWages;
+};
+const calculatePayroll = (employeeRecord) => {
+  return employeeRecord.reduce(function (accum, record) {
+    return accum + allWagesFor(record);
+  }, 0);
+};
